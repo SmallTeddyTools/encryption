dce8d4c11bbe47ed621faf7c411e155c8a971b78
SmallTeddy <zhangzeming1212@gmail.com>
2024-01-28 00:33:23 +0800

    fix: change config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('/encryption'),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
