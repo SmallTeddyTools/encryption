c69d27b5fa2fcc2282f60640862a9e2175ed10fe
SmallTeddy <zhangzeming1212@gmail.com>
2024-01-28 00:26:59 +0800

    feat: adaptive mobile terminal

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-import lock from './assets/lock.svg'
-import unlock from './assets/unlock.svg'
+import top from './assets/top.svg'
+import bottom from './assets/bottom.svg'
+import left from './assets/left.svg'
+import right from './assets/right.svg'
 import { decryptByDES, encryptByDES } from '@/utils/crypto'
 
 const encryption = ref('')
 const decryption = ref('')
+const bgWidth = computed(() => document.body.clientWidth)
 
 function getEncryption(str) {
   decryption.value = encryptByDES(str)
@@ -22,10 +25,14 @@ function getDecryption(str) {
     </div>
     <div class="btn-box">
       <el-button type="success" class="btn1" @click="getEncryption(encryption)">
-        加密 <img :src="lock" style="width: 20px; margin-left: 8px" alt="">
+        <img v-if="bgWidth <= 640" style="width: 20px; margin-right: 8px" :src="bottom" alt="Bottom">
+        <span>加密</span>
+        <img v-if="bgWidth > 640" :src="right" style="width: 20px; margin-left: 8px" alt="Right">
       </el-button>
       <el-button type="success" class="btn2" @click="getDecryption(decryption)">
-        <img :src="unlock" style="width: 20px; margin-right: 8px" alt=""> 解密
+        <img v-if="bgWidth > 640" style="width: 20px; margin-right: 8px" :src="left" alt="Left">
+        <span>解密</span>
+        <img v-if="bgWidth <= 640" style="width: 20px; margin-left: 8px" :src="top" alt="Top">
       </el-button>
     </div>
     <div class="decryption-text">
@@ -55,26 +62,46 @@ function getDecryption(str) {
     flex: 1;
 }
 
-.btn-box {
-    width: 120px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-content: center;
-    gap: 12px;
-}
-
 .bg .el-button + .el-button {
     margin-left: 0;
+}
+
+.btn-box {
+    display: flex;
+    gap: 12px;
 }
 
 .bg .el-textarea__inner,
 .bg .el-textarea {
     resize: none;
     background: transparent;
-    height: 100% !important;
+    height: 100% ;
     font-family: "-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,monospace";
     font-size: 20px;
     color: #fff;
+}
+
+/* 移动端 */
+@media (min-width: 320px) and (max-width: 640px) {
+    .bg {
+        flex-direction: column;
+    }
+
+    .btn-box {
+        height: 32px;
+        flex-direction: row;
+        justify-content: center;
+        align-content: center;
+    }
+}
+
+/* web 端 */
+@media (min-width: 640px) {
+    .btn-box {
+        width: 120px;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+    }
 }
 </style>
